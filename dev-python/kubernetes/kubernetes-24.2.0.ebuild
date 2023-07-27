stintel/gentoo-overlay

commit 22d622cb04976103a994c6e0f359f0600481e9d1

dev-python/kubernetes: drop SRC_URI
Fixes pkgdev PythonInlinePyPIURI warning.

Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

dev-python/kubernetes/kubernetes-24.2.0.ebuild
@@ -10,7 +10,6 @@ inherit distutils-r1
 
 DESCRIPTION="Python client for kubernetes"
 HOMEPAGE="https://github.com/kubernetes-client/python"
-SRC_URI="mirror://pypi/${PN:0:1}/${PN}/${P}.tar.gz"
 
 LICENSE="Apache-2.0"
 SLOT="0"

commit 8d2be1244ed12e85190b81b4b7f81fe107471dbd

dev-python/kubernetes: fix build
Fixes: 22d622cb0497 ("dev-python/kubernetes: drop SRC_URI")
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

dev-python/kubernetes/kubernetes-24.2.0.ebuild
@@ -2,11 +2,12 @@
 # Distributed under the terms of the GNU General Public License v2
 # $Header: $
 
-EAPI="7"
+EAPI="8"
 
-PYTHON_COMPAT=( python3_{8..11} )
+DISTUTILS_USE_PEP517=setuptools
+PYTHON_COMPAT=( python3_{10..11} )
 
-inherit distutils-r1
+inherit distutils-r1 pypi
 
 DESCRIPTION="Python client for kubernetes"
 HOMEPAGE="https://github.com/kubernetes-client/python"